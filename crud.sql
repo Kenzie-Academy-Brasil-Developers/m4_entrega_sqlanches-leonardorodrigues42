184be1e14c282145d6b734e24720eb1ec502c879
Leonardo Rodrigues <leonardo@pop-os.localdomain>
2022-10-05 14:37:03 -0300

    feat: projeto completo

--- crud.sql
+++ crud.sql
@@ -5,30 +5,75 @@
 
 -- 1)
 
-
+INSERT INTO clientes
+	(nome, lealdade)
+VALUES
+	('Georgia', 0);
 
 -- 2)
 
+SELECT id
+	FROM clientes
+    WHERE clientes.nome = 'Georgia';
 
+INSERT INTO pedidos
+	(status, cliente_id)
+VALUES
+	('Recebido', 6);
 
 -- 3)
 
+SELECT id
+	FROM pedidos
+    WHERE pedidos.cliente_id = 6;
 
+INSERT INTO produtos_pedidos 
+	(pedido_id, produto_id)
+VALUES 
+	(6, 1),
+	(6, 2),
+	(6, 6),
+	(6, 8),
+	(6, 8);
 
 -- Leitura
 
 -- 1)
 
-
+SELECT *
+	FROM clientes
+    JOIN pedidos
+    	ON clientes.id = pedidos.cliente_id
+    JOIN produtos_pedidos
+	    ON produtos_pedidos.pedido_id = pedidos.id
+    JOIN produtos
+    	ON produtos_pedidos.produto_id = produtos.id
+        WHERE clientes.id = 6;
 
 -- Atualização
 
 -- 1)
 
+SELECT sum(pts_de_lealdade)
+	FROM produtos, pedidos, clientes, produtos_pedidos
+    WHERE clientes.id = pedidos.cliente_id
+    AND pedidos.id = produtos_pedidos.pedido_id
+    AND produtos_pedidos.produto_id = produtos.id
+    AND clientes.id = 6;
 
+UPDATE clientes 
+	SET lealdade = 48
+    WHERE id = 6;
 
 -- Deleção
 
 -- 1)
 
+SELECT *
+	FROM clientes
+    WHERE nome = 'Marcelo';
 
+DELETE
+	FROM clientes
+    WHERE id = 4
+    AND nome = 'Marcelo';	
--- selects.sql
+++ selects.sql
@@ -2,11 +2,40 @@
 -- Seleções de dados
 
 -- 1)
-
+SELECT * FROM pedidos, produtos;
 -- 2)
+SELECT 
+	DISTINCT pedidos.id 
+FROM
+	pedidos, produtos, produtos_pedidos
+	WHERE produtos.id = produtos_pedidos.produto_id
+    AND produtos.nome = 'Fritas'
+    AND pedidos.id = produtos_pedidos.pedido_id;
 
 -- 3)
+SELECT 
+	clientes.nome as gostam_de_fritas 
+FROM
+	clientes, pedidos, produtos, produtos_pedidos
+	WHERE produtos.id = produtos_pedidos.produto_id
+    AND produtos.nome = 'Fritas'
+    AND pedidos.id = produtos_pedidos.pedido_id
+    AND clientes.id = pedidos.cliente_id;
 
 -- 4)
+SELECT 
+	sum(preço)
+FROM 
+	pedidos, clientes, produtos, produtos_pedidos
+	WHERE pedidos.id = produtos_pedidos.pedido_id
+    AND produtos.id = produtos_pedidos.produto_id
+    AND clientes.id = pedidos.cliente_id
+    AND clientes.nome = 'Laura';
 
 -- 5)
+SELECT 
+	distinct produtos.nome, count(produtos_pedidos.id)
+FROM
+	produtos, produtos_pedidos
+    WHERE produtos_pedidos.produto_id = produtos.id
+    GROUP BY produtos.nome;
